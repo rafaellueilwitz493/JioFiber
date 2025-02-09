0a42318de1358fffb39f09138fc80dbec116d1f6
Pawan <pawankoli264@gmail.com>
2025-02-10 05:13:12 +0530

    first commit

--- server/index.ts
+++ server/index.ts
@@ -169,7 +169,7 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  const PORT = 5000;
+  const PORT = 5001;
   server.close(() => {
     server.listen(PORT, "0.0.0.0", () => {
       log(`Server running on port ${PORT}`);
